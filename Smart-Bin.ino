92d9126d12d0ac1856c3de362d9ccc4400654dcd
Marta <37918515+marthabrp@users.noreply.github.com>
2020-06-14 17:28:43 -0300

    Add files via upload
    O projeto tem como principal objetivo automatizar a abertura de uma lixeira e facilitar o descarte de lixo.
    A automatização será feita através de um sensor de movimento, ligado a um Arduino que controla um micro servo, fazendo-o abrir a tampa da lixeira automaticamente quando o usuário se aproxima para descartar o lixo, ao se afastar da lixeira a tampa da mesma será fechada.

---
+++ Smart-Bin.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+int distancia = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+Servo servo_9;
+
+void setup()
+{
+  servo_9.attach(9);
+
+}
+
+void loop()
+{
+  distancia = 0.01723 * readUltrasonicDistance(7, 6);
+  servo_9.write(0);
+  delay(300); // Wait for 300 millisecond(s)
+  while (distancia <= 20) {
+    servo_9.write(180);
+    delay(900); // Wait for 300 millisecond(s)
+    distancia = 0.01723 * readUltrasonicDistance(7, 6);
+  }
+}
